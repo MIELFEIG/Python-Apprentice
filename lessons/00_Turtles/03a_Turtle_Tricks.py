44700d3ff01a4e2a921ede5fb5e94992aa7ecccf
MIELFEIG <mfeig@chasd.org>
2024-10-08 21:49:17 +0000

    panda and bamboo

--- lessons/00_Turtles/03a_Turtle_Tricks.py
+++ lessons/00_Turtles/03a_Turtle_Tricks.py
@@ -13,16 +13,62 @@ import turtle                           # Tell Python we want to work with the t
 turtle.setup (width=600, height=600)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
-tina.forward(100)
-tina.left(120) 
-tina.forward(100)  
-tina.left(120)   
-tina.forward(100)    
 
-# Use tina.forward() and tina.left() to draw a triangle
-# Make each side of the triangle a different color with 
-# tina.pencolor()
 
-... # Your code here
+def drawTree(treeWidth,numSegments):
+    tina.pencolor('green')
+    tina.pendown()
+    tina.left(90)
+    tina.forward(200)
+    tina.right(90)
+    tina.forward(treeWidth)
+    tina.right(90)
+    tina.forward(200)
+    tina.left(180)
+    tina.forward(400)
+    tina.left(90)
+    tina.forward(treeWidth)
+    tina.left(90)
+    tina.forward(400)
+    tina.penup()
+
+tina.penup()
+tina.goto(-200,-250)
+
+drawTree(40)
+
+
+
+tina.goto(-250, -250)
+tina.pencolor('green')
+tina.pendown()
+tina.left(180)
+tina.forward(200)
+tina.left(90)
+tina.forward(40)
+tina.left(90)
+tina.forward(200)
+tina.left(180)
+tina.forward(400)
+tina.right(90)
+tina.forward(40)
+tina.right(90)
+tina.forward(400)
+tina.left(180)
+tina.forward(500)
+tina.left(90)
+tina.forward(40)
+tina.left(90)
+tina.forward(200)
+
+tina.penup()
+tina.pencolor('black')
+tina.goto(0,100)
+
+tina.circle(50)
+
+
+
+
 
 turtle.exitonclick()                    # Close the window when we click on it
